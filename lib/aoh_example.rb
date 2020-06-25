68a101636070f7c90f05afaff6f3a27e3efe485f
Jacob Squier <jacob.squier@gmail.com>
2020-06-25 10:13:35 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,7 +21,7 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   arrHash = assembled_aoh()
-  return arrHash.sort
+  return arrHash.sort()
 end
 
 def aoh_lookup(aoh, row, key)
